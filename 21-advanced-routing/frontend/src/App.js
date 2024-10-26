6840961f761e9007ec697ccb694128e65815d978
selmasaltik <selmasaltiik@gmail.com>
2024-10-26 16:32:27 +0300

    Where Should loader() Code Be Stored?

--- 21-advanced-routing/frontend/src/App.js
+++ 21-advanced-routing/frontend/src/App.js
@@ -2,7 +2,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import RootLayout from './pages/Root';
 import ErrorPage from './pages/Error';
 import HomePage from './pages/Home';
-import EventsPage from './pages/Events';
+import EventsPage, { loader as eventsLoader } from './pages/Events';
 import EventDetailPage from './pages/EventDetail';
 import NewEventPage from './pages/NewEvent';
 import EditEventPage from './pages/EditEvent';
@@ -19,16 +19,11 @@ const router = createBrowserRouter([
         path: 'events', 
         element: <EventsRootLayout />, 
         children: [
-          { index: true, element: <EventsPage />, loader: async () => {
-            const response = await fetch('http://localhost:8080/events');
-
-            if (!response.ok) {
-              // ...
-            } else {
-              const resData = await response.json();
-              return resData.events;
-            }
-          } },
+          { 
+            index: true, 
+            element: <EventsPage />, 
+            loader: eventsLoader
+          },
           { path: ':eventId', element: <EventDetailPage /> },
           { path: 'new', element: <NewEventPage /> },
           { path: ':eventId/edit', element: <EditEventPage /> }
