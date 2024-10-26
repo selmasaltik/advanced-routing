346e8e644d962da92e28b71e7fd5af3f7dc80886
selmasaltik <selmasaltiik@gmail.com>
2024-10-26 16:13:19 +0300

    Data Fetching with a loader()

--- 21-advanced-routing/frontend/src/pages/Events.js
+++ 21-advanced-routing/frontend/src/pages/Events.js
@@ -10,20 +10,13 @@ function EventsPage() {
   useEffect(() => {
     async function fetchEvents() {
       setIsLoading(true);
-      const response = await fetch('http://localhost:8080/events');
-
-      if (!response.ok) {
-        setError('Fetching events failed.');
-      } else {
-        const resData = await response.json();
-        setFetchedEvents(resData.events);
-      }
+     
       setIsLoading(false);
     }
 
     fetchEvents();
   }, []);
-  
+
   return (
     <>
       <div style={{ textAlign: 'center' }}>
