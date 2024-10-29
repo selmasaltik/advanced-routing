93c284ffd1613284504d4487925946af1d66fca7
selmasaltik <selmasaltiik@gmail.com>
2024-10-29 19:08:22 +0300

    Behind-the-Scenes Work with useFetcher()

--- 21-advanced-routing/frontend/src/App.js
+++ 21-advanced-routing/frontend/src/App.js
@@ -1,17 +1,18 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-import RootLayout from './pages/Root';
-import ErrorPage from './pages/Error';
-import HomePage from './pages/Home';
-import EventsPage, { loader as eventsLoader } from './pages/Events';
-import EventDetailPage, { 
-  loader as eventDetailLoader,
-  action as deleteEventAction
-} from './pages/EventDetail';
-import NewEventPage from './pages/NewEvent';
 import EditEventPage from './pages/EditEvent';
+import ErrorPage from './pages/Error';
+import EventDetailPage, {
+  loader as eventDetailLoader,
+  action as deleteEventAction,
+} from './pages/EventDetail';
+import EventsPage, { loader as eventsLoader } from './pages/Events';
 import EventsRootLayout from './pages/EventsRoot';
+import HomePage from './pages/Home';
+import NewEventPage from './pages/NewEvent';
+import RootLayout from './pages/Root';
 import { action as manipulateEventAction } from './components/EventForm';
+import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
 
 const router = createBrowserRouter([
   {
@@ -20,41 +21,46 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
-      { 
-        path: 'events', 
-        element: <EventsRootLayout />, 
+      {
+        path: 'events',
+        element: <EventsRootLayout />,
         children: [
-          { 
-            index: true, 
-            element: <EventsPage />, 
-            loader: eventsLoader
+          {
+            index: true,
+            element: <EventsPage />,
+            loader: eventsLoader,
           },
           {
             path: ':eventId',
             id: 'event-detail',
             loader: eventDetailLoader,
             children: [
-              { 
-                index: true, 
+              {
+                index: true,
                 element: <EventDetailPage />,
-                action: deleteEventAction
+                action: deleteEventAction,
               },
-              { 
-                path: 'edit', 
+              {
+                path: 'edit',
                 element: <EditEventPage />,
-                action: manipulateEventAction
-              }
-            ]
+                action: manipulateEventAction,
+              },
+            ],
           },
-          { 
-            path: 'new', 
+          {
+            path: 'new',
             element: <NewEventPage />,
-            action: manipulateEventAction
-          }
-        ] 
-      }
-    ]
-  }
+            action: manipulateEventAction,
+          },
+        ],
+      },
+      {
+        path: 'newsletter',
+        element: <NewsletterPage />,
+        action: newsletterAction,
+      },
+    ],
+  },
 ]);
 
 function App() {
